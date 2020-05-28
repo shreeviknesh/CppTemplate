a24b9e0c236c142101cccf60b484bfff2d924817
shreeviknesh <luckyboy1998@gmail.com>
2020-05-28 20:16:28 +0530

    Changed build.sh to run_build

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+auto fibonacci(int n) -> int {
+    if (n == 1 || n == 0 || n > 10) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 int main() {
-    std::cout << "Hello, world!\n";
+    int n = 0;
+    std::cout << "Enter a number from 1-10: ";
+    std::cin >> n;
+    std::cout << "The " << n << "th fibonacci number is " << fibonacci(n) << "\n";
 }
